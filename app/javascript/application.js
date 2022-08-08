afa3a32036c5725bef1f1cb9357ca262d728b68c
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-08 10:12:45 +0300

    Add header to homepage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,11 +16,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import HelloMessage from "./components/App";
+import App from "./components/App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 document.addEventListener("DOMContentLoaded", () => {
-  root.render(<HelloMessage name="World" />);
+  root.render(<App />);
 });
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,3 +1,39 @@
 import React from "react";
-const HelloMessage = ({ name }) => <h1>Hello, {name}!</h1>;
-export default HelloMessage;
+import Container from "react-bootstrap/Container";
+
+import Header from "./Header";
+
+const App = () => (
+  <div>
+    <Header />
+    <Container>
+      <main role="main">
+        <div className="jumbotron">
+          <div className="col-sm-8 mx-auto">
+            <h3>Welcome to the Vending Machine.</h3>
+            <p>
+              To add your products to the vending machine, please sign up as a
+              seller
+            </p>
+            <p>
+              To buy products from the vending machine, please sign up as a
+              buyer
+            </p>
+            <p>
+              <a className="btn btn-primary" href="#login" role="button">
+                Sign up as seller &raquo;
+              </a>
+            </p>
+            <p>
+              <a className="btn btn-primary" href="#login" role="button">
+                Sign up as buyer &raquo;
+              </a>
+            </p>
+          </div>
+        </div>
+      </main>
+    </Container>
+  </div>
+);
+
+export default App;
