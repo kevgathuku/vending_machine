f9e519adfc007b5e711171fbdee7562d576c5db6
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-08 08:43:51 +0300

    Add authentication controller for creating a new User

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,14 @@
+class AuthenticationController < ApplicationController
+  # Skip checking headers for user creation
+  skip_before_action :authenticate_request
+
+  def authenticate
+    command = AuthenticateUser.call(params[:username], params[:password])
+
+    if command.success?
+      render json: { auth_token: command.result }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+  end
+end
