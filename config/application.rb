3c95731a95ee6615677bd89f427205cf374a6045
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-08 09:10:04 +0300

    Configure generator to be API only

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module VendingMachine
     #
     # config.time_zone = "Central Time (US & Canada)"
     config.eager_load_paths << Rails.root.join('lib')
+
+    # Only generate API resources
+    config.generators do |g|
+      g.api_only = true
+    end
   end
 end
