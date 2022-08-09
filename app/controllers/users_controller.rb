45d203d26cf841b0b09231b3cdd161a85eb96470
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-09 09:42:44 +0300

    Add user destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update]
-  before_action :authenticate_request, only: %i[show update]
+  before_action :set_user, only: %i[show update destroy]
+  before_action :authenticate_request, only: %i[show update destroy]
 
   def index
     @users = User.all
@@ -34,6 +34,10 @@ class UsersController < ApplicationController
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @user.destroy
   end
 
   private
