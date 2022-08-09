077e8e74130773f6d25b085d4a97a2c4b542a228
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-09 08:59:50 +0300

    Validate username is unique for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   enum role: { buyer: 0, seller: 1 }
 
+  validates :username, presence: true, uniqueness: true
+
   has_secure_password
 
   has_many :products, foreign_key: 'seller', inverse_of: 'seller'
