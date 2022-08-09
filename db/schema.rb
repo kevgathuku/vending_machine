d7c639fc7e50330c556be2f1cb5ed0667245d3b7
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-09 10:42:53 +0300

    Enable postgres extension in schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_08_08_062229) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "amount_available"
