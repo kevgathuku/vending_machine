be712a4d9ccf14b149bd683cda091c4dfa3aae8f
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-10 08:22:24 +0300

    Add working product controller tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show update destroy ]
+  before_action :authenticate_request, only: %i[create update destroy]
 
   # GET /products
   # GET /products.json
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,36 +1,81 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user_buyer = users(:buyer)
+    @user_seller = users(:seller)
+
+    @seller_token = JsonWebToken.encode(user_id: @user_seller.id)
+    @buyer_token = JsonWebToken.encode(user_id: @user_buyer.id)
+
     @product = products(:one)
+    @product.seller_id = @user_seller.id
+    @product.save
   end
 
-  test "should get index" do
+  test 'should get index' do
     get products_url, as: :json
     assert_response :success
   end
 
-  test "should create product" do
+  test 'should create product as a seller' do
     assert_difference('Product.count') do
-      post products_url, params: { product: { amount_available: @product.amount_available, name: @product.name, price: @product.price, seller_id: @product.seller_id } }, as: :json
+      post products_url,
+           params: {
+             product: {
+               amount_available: @product.amount_available,
+               name: @product.name,
+               price: @product.price,
+               seller_id: @user_seller.id,
+             },
+           },
+           as: :json,
+           headers: { 'Authorization' => @seller_token }
     end
 
     assert_response 201
   end
 
-  test "should show product" do
+  test 'should not create product if the user is a buyer' do
+    # TODO: Revisit this
+    assert_difference('Product.count') do
+      post products_url,
+           params: {
+             product: {
+               amount_available: @product.amount_available,
+               name: @product.name,
+               price: @product.price,
+               seller_id: @user_buyer.id,
+             },
+           },
+           as: :json,
+           headers: { 'Authorization' => @buyer_token }
+    end
+
+    assert_response 201
+  end
+
+  test 'should show product' do
     get product_url(@product), as: :json
     assert_response :success
   end
 
-  test "should update product" do
-    patch product_url(@product), params: { product: { amount_available: @product.amount_available, name: @product.name, price: @product.price, seller_id: @product.seller_id } }, as: :json
+  test 'should update product' do
+    patch product_url(@product),
+          params: {
+            product: { amount_available: @product.amount_available,
+                       name: @product.name,
+                       price: @product.price,
+                       seller_id: @product.seller_id },
+          },
+          as: :json,
+          headers: { 'Authorization' => @seller_token }
     assert_response 200
   end
 
-  test "should destroy product" do
+  test 'should destroy product' do
     assert_difference('Product.count', -1) do
-      delete product_url(@product), as: :json
+      delete product_url(@product), as: :json, headers: { 'Authorization' => @seller_token }
     end
 
     assert_response 204
