3f09c7d93b9e1ca6acf5f15ba2f21ee383084387
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-10 15:23:12 +0300

    Add product amount available validation and tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_buyer = create(:user, :buyer)
+    @user_seller = create(:user, :seller)
+    @other_seller = create(:user, :seller)
+  end
+
+  test 'should allow sellers to create products' do
+    product = build(:product, seller: @user_seller)
+    assert product.save
+  end
+
+  test 'should not allow buyers to create products' do
+    product = build(:product, seller: @user_buyer)
+    assert_not product.save
+  end
+
+  test 'should not allow negative amounts' do
+    product = build(:product, seller: @user_seller, amount_available: -1)
+    assert_not product.save
+  end
 end
