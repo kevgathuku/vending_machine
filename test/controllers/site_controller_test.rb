ffe0fc72525dd4b705a122c70915bc24c1fc80dc
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-10 12:42:30 +0300

    Add correct root url for site controller test

--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class SiteControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get site_index_url
+  test 'should get index' do
+    get root_url
     assert_response :success
   end
 end
