81e6edc601f777066b9daa83f4643c1b221994f5
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-24 10:03:05 +0300

    Ensure deposit only accepts valid amounts

--- app/controllers/deposit_controller.rb
+++ app/controllers/deposit_controller.rb
@@ -2,14 +2,21 @@ class DepositController < ApplicationController
   before_action :set_user, only: %i[deposit]
   before_action :authenticate_request, only: %i[deposit]
 
+  VALID_DEPOSIT_AMOUNTS = [5, 10, 20, 50, 100]
+
   def deposit
-    deposit_amount = deposit_params[:amount]
-    @user.with_lock do
-      # This block is called within a transaction
-      @user.deposit += deposit_amount
-      @user.save
+    amount = deposit_params[:amount]
+
+    if valid_deposit?(amount)
+      @user.with_lock do
+        # This block is called within a transaction
+        @user.deposit += amount
+        @user.save
+      end
+      render json: @user, status: :ok
+    else
+      render json: { error: 'Invalid deposit' }, status: :bad_request
     end
-    render json: @user, status: :ok
   end
 
   private
@@ -20,5 +27,9 @@ class DepositController < ApplicationController
 
   def deposit_params
     params.require(:deposit).permit(:amount)
+  end
+
+  def valid_deposit?(amount)
+    VALID_DEPOSIT_AMOUNTS.include?(amount)
   end
 end
--- test/controllers/deposit_controller_test.rb
+++ test/controllers/deposit_controller_test.rb
@@ -8,7 +8,7 @@ class DepositControllerTest < ActionDispatch::IntegrationTest
     @buyer_token = JsonWebToken.encode(user_id: @user_buyer.id)
   end
 
-  test 'should accept deposit from a buyer user' do
+  test 'should accept deposit from a buyer' do
     deposit_amount = 5
     initial_deposit = @user_buyer.deposit
 
@@ -21,7 +21,24 @@ class DepositControllerTest < ActionDispatch::IntegrationTest
          as: :json,
          headers: { 'Authorization' => @buyer_token }
 
-    assert @user_buyer.deposit, (initial_deposit + 5)
+    assert @user_buyer.deposit, (initial_deposit + deposit_amount)
     assert_response 200
+  end
+
+  test 'should not accept invalid deposit amount from a buyer' do
+    deposit_amount = 4
+    initial_deposit = @user_buyer.deposit
+
+    post user_deposit_path(@user_buyer.id),
+         params: {
+           deposit: {
+             amount: deposit_amount,
+           },
+         },
+         as: :json,
+         headers: { 'Authorization' => @buyer_token }
+
+    assert @user_buyer.deposit, initial_deposit
+    assert_response 400
   end
 end
