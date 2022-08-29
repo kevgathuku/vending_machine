ae1cea9c60189e23558cddea4666ce387d877c98
Kevin Gathuku <kevgathuku@gmail.com>
2022-08-29 11:39:22 +0300

    Show the logged in user info in the header

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,28 +1,43 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 import { LinkContainer } from "react-router-bootstrap";
 
-const Header = () => (
-  <Navbar bg="light" expand="lg">
-    <Container>
-      <LinkContainer to="/">
-        <Navbar.Brand>Home</Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          {/* Signed in as: <a href="#login">Mark Otto</a> */}
-          <LinkContainer to="/login">
-            <Nav.Link>Login</Nav.Link>
-          </LinkContainer>
-        </Navbar.Text>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+const Header = () => {
+  const [userName, setUserName] = useState("");
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (userInfo?.username) {
+      setUserName(userInfo.username);
+    }
+  });
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>Home</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            {userName ? (
+              <span>
+                Signed in as: <a href="profile"> {userName} </a>
+              </span>
+            ) : (
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            )}
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
