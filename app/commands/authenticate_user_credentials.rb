aac9b237635944f6b1c67ede634d25a6495be50b
Kevin Gathuku <kevgathuku@gmail.com>
2022-09-08 20:44:55 +0300

    Add dry-based command to authenticate user

---
+++ app/commands/authenticate_user_credentials.rb
@@ -0,0 +1,39 @@
+require 'dry/monads'
+
+class AuthenticateUserCredentials
+  include Dry::Monads[:result, :do]
+
+  def call(username:, password:)
+    @username = username
+    @password = password
+
+    existing_user = yield fetch_user
+    user = yield validate_password(user: existing_user)
+    token = JsonWebToken.encode(user_id: user.id)
+
+    Success(token)
+  end
+
+  private
+
+  attr_reader :username, :password
+
+  def fetch_user
+    user = User.find_by(username: username)
+
+    if user
+      Success(user)
+    else
+      Failure('User not found')
+    end
+  end
+
+  def validate_password(user:)
+    result = user.authenticate(password)
+    if result
+      Success(result)
+    else
+      Failure('Invalid credentials')
+    end
+  end
+end
