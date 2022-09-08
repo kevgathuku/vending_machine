e928c31db13e4c6594a33ce166c4cd8aace9d9f6
Kevin Gathuku <kevgathuku@gmail.com>
2022-09-08 20:48:08 +0300

    Use new authentication command for user auth

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,15 +1,22 @@
+require 'dry/matcher/result_matcher'
+
 class AuthenticationController < ApplicationController
   # Skip checking headers for user creation / login
   skip_before_action :authenticate_request
 
   def authenticate
-    command = AuthenticateUser.call(params[:username], params[:password])
-    user = User.find_by(username: params[:username])
+    command = AuthenticateUserCredentials.new
+    user_token = command.call(username: params[:username], password: params[:password])
 
-    if command.success?
-      render json: { auth_token: command.result, user: user.as_json(only: [:id, :role, :username]) }
-    else
-      render json: { error: command.errors }, status: :unauthorized
+    Dry::Matcher::ResultMatcher.(user_token) do |m|
+      m.success do |result|
+        user = User.find_by(username: params[:username])
+        render json: { auth_token: result, user: user.as_json(only: [:id, :role, :username]) }
+      end
+
+      m.failure do |err|
+        render json: { error: err }, status: :unauthorized
+      end
     end
   end
 end
