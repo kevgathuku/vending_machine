163c2ec840d26a3fe6d09dbd02019317dbc13d20
Kevin Gathuku <kevgathuku@gmail.com>
2023-04-08 13:35:01 +0300

    Use subject in tests

--- spec/commands/authenticate_user_credentials_spec.rb
+++ spec/commands/authenticate_user_credentials_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe AuthenticateUserCredentials do
+  subject(:result) { described_class.new.call(user_params) }
+
   context 'with valid user credentials' do
-    let(:username) { 'qwerty' }
-    let(:password) { 'poiuytr' }
-    let(:user_params) { { username: username, password: password } }
+    let(:user_params) { { username: 'qwerty', password: 'poiuytr' } }
     let!(:user) { User.create(user_params) }
-    let(:result) { described_class.new.call(user_params) }
 
     it 'returns a token' do
       expect(result).to be_success
